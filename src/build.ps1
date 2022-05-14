c7eb3202e9faa7a7dfef8cc54d3ecc3a0bb2958d
Viktor Pti <qbit86@gmail.com>
2022-05-14 12:12:26 +0300

    Add capturing commit height

--- src/build.ps1
+++ src/build.ps1
@@ -1,4 +1,6 @@
+$headHeight = git rev-list --count HEAD
+$outputFilename = "out/resume-viktor-tselov-${headHeight}.pdf"
 md -Force out/ | Out-Null
 pandoc -d defaults/base.yaml -d defaults/html.yaml
 pandoc -d defaults/base.yaml -d defaults/pdf.yaml
-pagedjs-cli -i out/__resume-viktor-tselov.html -o out/resume-viktor-tselov.pdf
+pagedjs-cli -i out/__resume-viktor-tselov.html -o $outputFilename
