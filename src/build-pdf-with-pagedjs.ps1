e9ba26e4cffb66c7032b695df36bb7409737ce57
Viktor Pti <qbit86@gmail.com>
2022-07-14 23:39:13 +0300

    Reorder declarations

--- src/build-pdf-with-pagedjs.ps1
+++ src/build-pdf-with-pagedjs.ps1
@@ -1,7 +1,7 @@
 $inputFilename = "$PSScriptRoot\resume-viktor-tselov.html"
-$inputRelative = Resolve-Path -Relative $inputFilename
 $outputDir = "$PSScriptRoot\out"
 mkdir -Force $outputDir | Out-Null
 $headHeight = git rev-list --count HEAD
 $outputPdf = "$outputDir\resume-viktor-tselov-$headHeight.pdf"
+$inputRelative = Resolve-Path -Relative $inputFilename
 pagedjs-cli -i $inputRelative -o $outputPdf
