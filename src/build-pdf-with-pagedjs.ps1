be948cb1c066f1b70c0fa2c5bc7e3da983a2094f
Viktor T <qbit86@gmail.com>
2022-07-14 17:57:42 +0300

    Add workaround for absolute path for pagedjs and puppeteer

--- src/build-pdf-with-pagedjs.ps1
+++ src/build-pdf-with-pagedjs.ps1
@@ -1,6 +1,7 @@
 $inputFilename = "$PSScriptRoot\resume-viktor-tselov.html"
+$inputRelative = Resolve-Path -Relative $inputFilename
 $outputDir = "$PSScriptRoot\out"
 mkdir -Force $outputDir | Out-Null
 $headHeight = git rev-list --count HEAD
 $outputFilename = "$outputDir\resume-viktor-tselov-$headHeight.pdf"
-pagedjs-cli -i $inputFilename -o $outputFilename
+pagedjs-cli -i $inputRelative -o $outputFilename
