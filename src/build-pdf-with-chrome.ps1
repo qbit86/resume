65a02e67e1386e4aba868232aa305d899fddcfa8
Viktor T <qbit86@gmail.com>
2022-07-14 18:25:00 +0300

    Refactor scripts for building PDF

--- src/build-pdf-with-chrome.ps1
+++ src/build-pdf-with-chrome.ps1
@@ -1,6 +1,6 @@
-$headHeight = git rev-list --count HEAD
 $inputFilename = "$PSScriptRoot\resume-viktor-tselov.html"
 $outputDir = "$PSScriptRoot\out"
 mkdir -Force $outputDir | Out-Null
-$outputFilename = "$outputDir\resume-viktor-tselov-$headHeight.pdf"
-& "$env:ProgramFiles\Google\Chrome\Application\chrome.exe" --headless --run-all-compositor-stages-before-draw --print-to-pdf-no-header --print-to-pdf=$outputFilename $inputFilename
+$headHeight = git rev-list --count HEAD
+$outputPdf = "$outputDir\resume-viktor-tselov-$headHeight.pdf"
+& "$env:ProgramFiles\Google\Chrome\Application\chrome.exe" --headless --run-all-compositor-stages-before-draw --print-to-pdf-no-header --print-to-pdf=$outputPdf $inputFilename
--- src/build-pdf-with-pagedjs.ps1
+++ src/build-pdf-with-pagedjs.ps1
@@ -3,5 +3,5 @@ $inputRelative = Resolve-Path -Relative $inputFilename
 $outputDir = "$PSScriptRoot\out"
 mkdir -Force $outputDir | Out-Null
 $headHeight = git rev-list --count HEAD
-$outputFilename = "$outputDir\resume-viktor-tselov-$headHeight.pdf"
-pagedjs-cli -i $inputRelative -o $outputFilename
+$outputPdf = "$outputDir\resume-viktor-tselov-$headHeight.pdf"
+pagedjs-cli -i $inputRelative -o $outputPdf
