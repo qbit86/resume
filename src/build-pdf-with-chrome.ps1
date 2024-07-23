075b98816138f93fab331af2fd9d5e43023e064f
Viktor Ptitselov <qbit86@gmail.com>
2024-07-23 03:57:46 +0300

    Fix command line argument for Chrome

--- src/build-pdf-with-chrome.ps1
+++ src/build-pdf-with-chrome.ps1
@@ -4,4 +4,4 @@ $outputDir = "$PSScriptRoot\out"
 mkdir -Force $outputDir | Out-Null
 $headHeight = git rev-list --count HEAD
 $outputPdf = "$outputDir\$inputBasename-$headHeight.pdf"
-& "$env:ProgramFiles\Google\Chrome\Application\chrome.exe" --headless --run-all-compositor-stages-before-draw --print-to-pdf-no-header --print-to-pdf=$outputPdf $inputFilename
+& "$env:ProgramFiles\Google\Chrome\Application\chrome.exe" --headless --run-all-compositor-stages-before-draw --no-pdf-header-footer --print-to-pdf=$outputPdf $inputFilename
