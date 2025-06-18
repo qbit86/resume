1607f387373a7c657f1b41624a92156c5cfd7faa
Viktor Tselov <viktor.tselov@gmail.com>
2025-06-18 03:04:25 +0300

    Update PDF output filename format to include 'gamedev' for clarity

--- src/build-pdf-with-chrome.ps1
+++ src/build-pdf-with-chrome.ps1
@@ -3,5 +3,5 @@ $inputFilename = "$PSScriptRoot\$inputBasename.html"
 $outputDir = "$PSScriptRoot\out"
 mkdir -Force $outputDir | Out-Null
 $headHeight = git rev-list --count HEAD
-$outputPdf = "$outputDir\$inputBasename-$headHeight.pdf"
+$outputPdf = "$outputDir\$inputBasename-gamedev-$headHeight.pdf"
 & "$env:ProgramFiles\Google\Chrome\Application\chrome.exe" --headless --run-all-compositor-stages-before-draw --no-pdf-header-footer --print-to-pdf=$outputPdf $inputFilename
