1f5334321b20a5ca343bcd2664a2ece6f98f9dee
Viktor Tselov <viktor.tselov@gmail.com>
2025-07-02 00:00:25 +0300

    Remove obsolete scripts for HTML and PDF generation

--- src/build-html-with-pandoc.ps1
+++
@@ -1,7 +0,0 @@
-$inputBasename = "resume-viktor-tselov"
-$inputFilename = "$PSScriptRoot\$inputBasename.html"
-$outputDir = "$PSScriptRoot\out"
-mkdir -Force $outputDir | Out-Null
-$headHeight = git rev-list --count HEAD
-$outputHtml = "$outputDir\$inputBasename-$headHeight.html"
-pandoc -d "$PSScriptRoot\defaults\html5.yaml" --output=$outputHtml $inputFilename
--- src/build-pdf-with-pagedjs.ps1
+++
@@ -1,8 +0,0 @@
-$inputBasename = "resume-viktor-tselov"
-$inputFilename = "$PSScriptRoot\$inputBasename.html"
-$outputDir = "$PSScriptRoot\out"
-mkdir -Force $outputDir | Out-Null
-$headHeight = git rev-list --count HEAD
-$outputPdf = "$outputDir\$inputBasename-$headHeight.pdf"
-$inputRelative = Resolve-Path -Relative $inputFilename
-pagedjs-cli -i $inputRelative -o $outputPdf
